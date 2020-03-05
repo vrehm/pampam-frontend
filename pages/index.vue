7797f0cc0731fe19462cfe3cd6e021c1e9d35ca3
Vincent Rehm <vincent.rehm@ifsalpha.com>
2020-03-05 20:10:59 +0100

    update index layout with dynamic categories per article

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container mx-auto">
     <div class="flex flex-wrap justify-start mt-4 mb-16">
-      <h2 class="subtitle w-full">
+      <h2 class="text-4xl m-2 w-full">
         Nuxt blog test @Bivwak!
       </h2>
 
@@ -21,19 +21,14 @@
             {{ article.content }}
           </p>
         </div>
-        <div class="px-6 py-4">
+        <div class="flex justify-center items-end my-4">
           <span
-            class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
-            >#photography</span
+            v-for="category in article.categories"
+            :key="category.id"
+            class="inline-block bg-gray-200 rounded-full text-sm font-semibold text-gray-700 m-1 py-1 px-2"
           >
-          <span
-            class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
-            >#travel</span
-          >
-          <span
-            class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700"
-            >#winter</span
-          >
+            {{ category.name }}
+          </span>
         </div>
       </div>
 
@@ -65,40 +60,4 @@ export default {
 }
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style></style>
