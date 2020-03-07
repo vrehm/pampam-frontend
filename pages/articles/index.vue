a4c21b1e0b6dabde4bdeab3870490a403d1f3aec
Vincent Rehm <vincent.rehm@ifsalpha.com>
2020-03-07 20:43:21 +0100

    update blog navigation between pages

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -3,29 +3,29 @@
     <div class="flex flex-wrap justify-start mt-4 mb-16">
       <div class="w-full flex justify-between items-center">
         <h2 class="text-4xl m-2 flex-1">
-          Nuxt blog test @Bivwak!
+          Articles index - Nuxt blog test @Bivwak!
         </h2>
         <nuxt-link
-          to="/articles/"
+          to="/"
           class="bg-transparent hover:bg-gray-500 text-gray-700 font-semibold hover:text-white py-2 px-4 border border-gray-500 hover:border-transparent rounded"
         >
-          See all Articles
+          Back to homepage
         </nuxt-link>
       </div>
 
-      <div
+      <!-- <div
         v-for="category in categories"
         :key="category.id"
         class="w-full flex flex-wrap justify-start m-2"
       >
-        <span class="text-2xl w-full my-2">{{ category.name }}</span>
-        <div v-for="article in category.articles" :key="article.id">
-          <article-card
-            :article="articles[article.id - 1]"
-            class="max-w-xs rounded overflow-hidden shadow-lg m-2"
-          />
-        </div>
+        <span class="text-2xl w-full my-2">{{ category.name }}</span> -->
+      <div v-for="article in articles" :key="article.id">
+        <article-card
+          :article="article"
+          class="max-w-xs rounded overflow-hidden shadow-lg m-2"
+        />
       </div>
+      <!-- </div> -->
     </div>
   </div>
 </template>
