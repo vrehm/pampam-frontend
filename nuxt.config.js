7d29563d7c13cd15ce671d6d821ee759d1ffa014
Vincent REHM <rehmvincent@gmail.com>
2020-03-17 19:41:34 +0100

    website password protected

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,80 +1,95 @@
+/* eslint-disable prettier/prettier */
 require('dotenv').config()
 
 export default {
-  env: {
-    assetsBaseUrl:
-      process.env.MODE === 'production' || process.env.NODE_ENV === 'production'
-        ? ''
-        : 'http://localhost:1337'
-  },
-  mode: 'universal',
-  /*
-   ** Headers of the page
-   */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
-      }
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
-  },
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: '#fff' },
-  /*
-   ** Global CSS
-   */
-  css: [],
-  /*
-   ** Plugins to load before mounting the App
-   */
-  plugins: [],
-  /*
-   ** Nuxt.js dev-modules
-   */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
-    // Doc: https://github.com/nuxt-community/stylelint-module
-    '@nuxtjs/stylelint-module',
-    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/dotenv'
-  ],
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-    // Doc: https://github.com/nuxt-community/dotenv-module
-    '@nuxtjs/dotenv'
-  ],
-  /*
-   ** Axios module configuration
-   ** See https://axios.nuxtjs.org/options
-   */
-  axios: {
-    baseURL:
-      process.env.MODE === 'production' || process.env.NODE_ENV === 'production'
-        ? process.env.BACKEND_URL
-        : 'http://localhost:1337'
-  },
-  /*
-   ** Build configuration
-   */
-  build: {
+    router: {
+        middleware: ['password-protect']
+    },
+    env: {
+        assetsBaseUrl: process.env.MODE === 'production' || process.env.NODE_ENV === 'production' ?
+            '' :
+            'http://localhost:1337'
+    },
+    mode: 'universal',
     /*
-     ** You can extend webpack config here
+     ** Headers of the page
      */
-    extend(config, ctx) {}
-  }
+    head: {
+        title: process.env.npm_package_name || '',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            {
+                hid: 'description',
+                name: 'description',
+                content: process.env.npm_package_description || ''
+            }
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    },
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: { color: '#fff' },
+    /*
+     ** Global CSS
+     */
+    css: [],
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [],
+    /*
+     ** Nuxt.js dev-modules
+     */
+    buildModules: [
+        // Doc: https://github.com/nuxt-community/eslint-module
+        '@nuxtjs/eslint-module',
+        // Doc: https://github.com/nuxt-community/stylelint-module
+        '@nuxtjs/stylelint-module',
+        // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+        '@nuxtjs/tailwindcss',
+        '@nuxtjs/dotenv',
+        'nuxt-password-protect'
+    ],
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [
+        // Doc: https://axios.nuxtjs.org/usage
+        '@nuxtjs/axios',
+        '@nuxtjs/pwa',
+        // Doc: https://github.com/nuxt-community/dotenv-module
+        '@nuxtjs/dotenv',
+        'nuxt-password-protect'
+    ],
+    passwordProtect: {
+        formPath: '/password',
+        password: 'hello-world',
+        tokenSeed: 101010,
+        queryString: '_pw',
+        cookieName: '_password',
+        cookie: {
+            prefix: '',
+            expires: 5
+        }
+    },
+    /*
+     ** Axios module configuration
+     ** See https://axios.nuxtjs.org/options
+     */
+    axios: {
+        baseURL: process.env.MODE === 'production' || process.env.NODE_ENV === 'production' ?
+            process.env.BACKEND_URL :
+            'http://localhost:1337'
+    },
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {}
+    }
 }
---
+++ pages/password.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="container mx-auto flex justify-center items-center">
+    <div v-if="isAuthorised">
+      <h1 class="pb-4 text-2xl">Looks like you're already logged in</h1>
+
+      <p class="pb-4 text-xl">
+        Either logout or go to the password protected area
+      </p>
+      <div class="flex justify-between">
+        <a class="text-gray-500 underline" href="/" @click="removeAuthorisation"
+          >Log out</a
+        >
+        <a class="text-gray-500 underline" href="/"
+          >Go to password protected area</a
+        >
+      </div>
+    </div>
+    <div v-else>
+      <h1 class="pb-4 text-2xl">Log in</h1>
+      <p class="pb-4 text-xl">
+        Enter in your password, you can do so by filling in the form below.
+      </p>
+
+      <div>
+        <form class="flex justify-between">
+          <input
+            v-model="methodLoginValue"
+            class="w-2/3"
+            type="password"
+            placeholder="Your password for the method login approach"
+          />
+
+          <a class="text-gray-500 underline" href="/" @click="loginUser()"
+            >Login</a
+          >
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      methodLoginValue: '',
+      isAuthorised: false
+    }
+  },
+  mounted() {
+    this.isAuthorised = this.$passwordProtect.isAuthorised()
+  },
+
+  methods: {
+    loginUser() {
+      this.$passwordProtect.authorise(this.methodLoginValue)
+      this.isAuthorised = this.$passwordProtect.isAuthorised()
+      this.$router.push('/')
+    },
+    removeAuthorisation() {
+      this.$passwordProtect.removeAuthorisation()
+      this.isAuthorised = this.$passwordProtect.isAuthorised()
+    }
+  }
+}
+</script>
+
+<style></style>
