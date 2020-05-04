8808c074422d514c0c509dd63bcaf1b43158e24b
thibaut dumontet <dumontet.thibaut@gmail.com>
2020-05-04 07:24:37 +0200

    Update tailwind config and package according to tailwind-ui requirements

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,25 @@
  ** Docs: https://tailwindcss.com/docs/configuration
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  theme: {},
+  prefix: 'tw-',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
   variants: {},
-  plugins: []
+  plugins: [
+    require('@tailwindcss/ui')({
+      // See https://tailwindui.com/documentation#configuring-sidebar-breakpoints
+      // Turning this off for now. This may be buggy as it makes the hero sections like the "with wide angled image on right" look funky
+      // https://tailwindui.com/components/marketing/sections/heroes
+      // layout: 'sidebar',
+    })
+  ]
 }
