10825006685f7718d25fdc2db9840d702011ca02
thibaut dumontet <dumontet.thibaut@gmail.com>
2020-05-04 09:40:16 +0200

    Create misc components with tailwind UI for homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,29 @@
 <template>
   <div>
     <hero-section />
+    <newsletter-section />
+    <feature-section />
+    <testimonials />
+    <logo-clouds />
+    <footer-section />
   </div>
 </template>
 
 <script>
 import HeroSection from '~/components/HeroSection.vue'
+import NewsletterSection from '~/components/NewsletterSection.vue'
+import FooterSection from '~/components/FooterSection.vue'
+import FeatureSection from '~/components/FeatureSection.vue'
+import Testimonials from '~/components/Testimonials.vue'
+import LogoClouds from '~/components/LogoClouds.vue'
 export default {
   components: {
-    HeroSection
+    HeroSection,
+    NewsletterSection,
+    FooterSection,
+    FeatureSection,
+    Testimonials,
+    LogoClouds
   }
 }
 </script>
