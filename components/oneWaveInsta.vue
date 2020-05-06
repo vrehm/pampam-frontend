3efbc41bada22e7f6824bf1fd6ee9ebbf725d2ff
Vincent Rehm <vincentrehm@yahoo.fr>
2020-05-06 19:42:59 +0200

    setup of this snippet https://gitlab.com/snippets/1957175 + getting children images of a carousel album

---
+++ components/oneWaveInsta.vue
@@ -0,0 +1,125 @@
+<template>
+  <div :class="containerClass">
+    <slot name="loading" :loading="loading" />
+    <slot
+      v-for="(feed, index) in feeds"
+      :index="index"
+      :feed="feed"
+      name="feeds"
+    />
+    <slot name="error" :error="error" />
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  props: {
+    /*
+     * Instagram access token.
+     */
+    token: {
+      type: String,
+      required: true
+    },
+    /*
+     * Instagram user id.
+     */
+    userId: {
+      type: String,
+      required: true
+    },
+    /*
+     * Media Fields (see https://developers.facebook.com/docs/instagram-basic-display-api/reference/media#fields)
+     */
+    fields: {
+      type: String,
+      required: true
+    },
+    /*
+     * Number of posts rendered.
+     */
+    count: {
+      type: Number,
+      default: 50
+    },
+    /*
+     * Kinds of media to filter (Can be IMAGE, VIDEO, or CAROUSEL_ALBUM.).
+     */
+    mediatypes: {
+      type: Array,
+      required: true
+    },
+    // Class for container div
+    containerClass: {
+      type: String,
+      default: '',
+      required: false
+    }
+  },
+  data: () => ({
+    error: null,
+    loading: false,
+    feeds: []
+  }),
+  mounted() {
+    this.getUserFeed()
+  },
+  methods: {
+    getUserFeed() {
+      const endpoint = 'https://graph.instagram.com/' + this.userId + '/media'
+      this.loading = true
+
+      axios
+        .get(endpoint, {
+          params: { access_token: this.token, fields: this.fields }
+        })
+        .then((response) => {
+          this.loading = false
+
+          if (response.status === 400) this.error = response.error.message
+
+          if (response.status === 200) {
+            for (const n in response.data.data) {
+              if (this.mediatypes.includes(response.data.data[n].media_type)) {
+                const data = response.data.data[n]
+
+                if (data.children) {
+                  const caption = data.caption
+                  const children = data.children.data
+                  delete data.children
+                  for (let index = 0; index < children.length; index++) {
+                    const child = children[index].id
+                    const endpoint = 'https://graph.instagram.com/' + child
+
+                    axios
+                      .get(endpoint, {
+                        params: {
+                          access_token: this.token,
+                          fields: 'media_url,media_type'
+                        }
+                      })
+                      .then((response) => {
+                        const igChild = response
+                        igChild.caption = caption
+                        this.feeds.push(igChild)
+                      })
+                  }
+                }
+
+                this.feeds.push(data)
+
+                if (this.feeds.length >= this.count) {
+                  return
+                }
+              }
+            }
+          }
+        })
+        .catch((error) => {
+          throw error
+        })
+    }
+  }
+}
+</script>
---
+++ pages/feed.vue
@@ -0,0 +1,51 @@
+<template>
+  <oneWaveInsta
+    :token="token"
+    :user-id="userId"
+    fields="media_url,media_type,caption,children"
+    :mediatypes="['IMAGE', 'CAROUSEL_ALBUM']"
+    :count="50"
+  >
+    <template v-slot:loading="props">
+      <h1 v-if="props.loading" class="fancy-loading">
+        Loading, please wait...
+      </h1>
+    </template>
+
+    <template v-slot:feeds="props">
+      <div>
+        <img :src="props.feed.media_url" />
+        <div :text="props.feed.caption" />
+        <div v-if="props.feed.children">
+          <div
+            v-for="(child, index) in props.feed.children.data"
+            :key="index"
+            :child="child"
+          >
+            <span>{{ child.id }}</span>
+          </div>
+        </div>
+      </div>
+    </template>
+
+    <template v-slot:error="props">
+      <div class="fancy-alert">{{ props.error }}</div>
+    </template>
+  </oneWaveInsta>
+</template>
+
+<script>
+import oneWaveInsta from '~/components/oneWaveInsta.vue'
+
+export default {
+  components: {
+    oneWaveInsta
+  },
+  data: () => ({
+    token: process.env.IG_TOKEN,
+    userId: process.env.IG_USER_ID
+  })
+}
+</script>
+
+<style></style>
