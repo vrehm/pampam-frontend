ae9b19a322c69e35c66c5b74f951df6dc7e880f6
kyldex <raphael.giagnorio@gmail.com>
2020-05-15 00:50:22 +0200

    home hero section desktop xl

--- components/HomeHeroSection.vue
+++ components/HomeHeroSection.vue
@@ -1,11 +1,11 @@
 <template>
-  <section class="bg-gray-50 overflow-hidden">
-    <div class="relative pt-10 pb-12 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32">
+  <section class="background-custom h-screen overflow-hidden">
+    <div class="pt-48 pb-12 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-28">
       <div class="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-64">
-        <h1 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-tight md:text-6xl">
+        <h1 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-100 sm:text-5xl sm:leading-tight md:text-6xl">
           Brocante poétique
           <br />
-          <span class="text-gray-500">pour intérieurs singuliers</span>
+          <span class="text-yellow-500">pour intérieurs singuliers</span>
         </h1>
       </div>
     </div>
@@ -16,4 +16,11 @@
 export default {}
 </script>
 
-<style></style>
+<style>
+.background-custom {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5) 10%, rgba(0, 0, 0, 0.3) 90%), url('/img/home/hero-home2000x1500.jpg');
+  background-position: center, center;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
