e132a31fb0c4b096b739805406b89916deef1a1c
kyldex <raphael.giagnorio@gmail.com>
2020-05-14 19:21:42 +0200

    Navbar position absolute test

--- components/HomeHeroSection.vue
+++ components/HomeHeroSection.vue
@@ -1,7 +1,7 @@
 <template>
-  <section class="relative bg-gray-50 overflow-hidden">
-    <div class="relative pt-6 pb-12 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32">
-      <div class="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
+  <section class="bg-gray-50 overflow-hidden">
+    <div class="relative pt-10 pb-12 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32">
+      <div class="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-64">
         <h1 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-tight md:text-6xl">
           Brocante poétique
           <br />
