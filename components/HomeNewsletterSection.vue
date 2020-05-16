6e5993c10ecc6f1a0b9110c95732c2a4c3d4eace
kyldex <raphael.giagnorio@gmail.com>
2020-05-16 02:09:28 +0200

    newsletter V1 integration

---
+++ components/HomeNewsletterSection.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="bg-white">
+    <div class="max-w-4xl mx-auto px-4 py-12 sm:px-6 lg:py-16 lg:px-8">
+      <div class="flex flex-col items-center px-6 py-6 bg-gray-700 rounded-lg md:py-12 md:px-12 lg:py-16 lg:px-16">
+        <div class="text-center xl:flex-1">
+          <h2 class="text-2xl leading-8 font-extrabold tracking-tight text-white sm:text-3xl sm:leading-9">
+            La lettre !
+          </h2>
+          <p class="mt-3 max-w-xl text-lg leading-6 text-gray-100">
+            Tout les mois, recevez un billet inspirant, mes nouvelles trouvailles et des avants-premières directement dans votre boîte mail !
+          </p>
+        </div>
+        <div class="mt-8 sm:w-full sm:max-w-md">
+          <form class="sm:flex">
+            <input aria-label="Email address" type="email" required class="appearance-none w-full px-5 py-3 border border-transparent text-base leading-6 rounded-md text-gray-900 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 transition duration-150 ease-in-out" placeholder="Votre e-mail" />
+            <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3 sm:flex-shrink-0">
+              <button class="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-gray-500 hover:bg-indigo-400 focus:outline-none focus:bg-indigo-400 transition duration-150 ease-in-out">
+                PAM ! Je m'abonne
+              </button>
+            </div>
+          </form>
+          <p class="mt-3 text-sm leading-5 text-gray-100">
+            Nous nous soucions de la protection de vos données. Lisez notre
+            <a href="#" class="text-gray-300 font-medium underline">
+              Politique de confidentialité.
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
