0b3f9a49af124c4878203fc71170e43f16d5aed9
Vincent REHM <vincentrehm@yahoo.fr>
2020-05-16 19:47:33 +0200

    test

--- components/ArticleShow.vue
+++ components/ArticleShow.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="my-4">
     <img :src="assetsBaseUrl + article.image.url" :alt="article.title" />
-    <div class="text-gray-700 text-base break-normal my-4 leading-loose">
+    <div class="text-gray-700  text-base break-normal my-4 leading-loose">
       {{ article.content }}
     </div>
     <div class="flex justify-center items-end my-4">
