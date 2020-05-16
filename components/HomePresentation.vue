8eb951fa96685fc37b21d5816c637cf0ed3abfbb
kyldex <raphael.giagnorio@gmail.com>
2020-05-17 00:05:26 +0200

    incorporate zondicons svg

--- components/HomePresentation.vue
+++ components/HomePresentation.vue
@@ -1,92 +1,105 @@
 <template>
-  <div class="py-12 bg-white">
-    <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="mt-10">
-        <ul class="md:grid md:grid-cols-2 md:col-gap-8 md:row-gap-10">
-          <li>
-            <div class="flex">
-              <div class="flex-shrink-0">
-                <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                  <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
-                  </svg>
-                </div>
-              </div>
-              <div class="ml-4">
-                <h5 class="text-lg leading-6 font-medium text-gray-900">
-                  Competitive exchange rates
-                </h5>
-                <p class="mt-2 text-base leading-6 text-gray-500">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
-                </p>
-              </div>
+  <section class="py-12 bg-white">
+    <div class="md:flex md:justify-center max-w-screen-xl mt-10 mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="md:pr-4">
+        <div class="mb-10">
+          <h2 class="text-center text-lg leading-6 font-medium text-gray-900">
+            Who is Pam Pam?
+          </h2>
+        </div>
+
+        <div class="flex mb-10">
+          <div class="flex-shrink-0">
+            <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+              <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+              </svg>
             </div>
-          </li>
-          <li class="mt-10 md:mt-0">
-            <div class="flex">
-              <div class="flex-shrink-0">
-                <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                  <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
-                  </svg>
-                </div>
-              </div>
-              <div class="ml-4">
-                <h5 class="text-lg leading-6 font-medium text-gray-900">
-                  No hidden fees
-                </h5>
-                <p class="mt-2 text-base leading-6 text-gray-500">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
-                </p>
-              </div>
+          </div>
+          <div class="ml-4">
+            <h5 class="text-lg leading-6 font-medium text-gray-900">
+              Competitive exchange rates
+            </h5>
+            <p class="mt-2 text-base leading-6 text-gray-500">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+            </p>
+          </div>
+        </div>
+
+        <div class="flex">
+          <div class="flex-shrink-0">
+            <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+              <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
+              </svg>
             </div>
-          </li>
-          <li class="mt-10 md:mt-0">
-            <div class="flex">
-              <div class="flex-shrink-0">
-                <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                  <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
-                  </svg>
-                </div>
-              </div>
-              <div class="ml-4">
-                <h5 class="text-lg leading-6 font-medium text-gray-900">
-                  Transfers are instant
-                </h5>
-                <p class="mt-2 text-base leading-6 text-gray-500">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
-                </p>
-              </div>
+          </div>
+          <div class="ml-4">
+            <h5 class="text-lg leading-6 font-medium text-gray-900">
+              No hidden fees
+            </h5>
+            <p class="mt-2 text-base leading-6 text-gray-500">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="md:pl-4 border-presentation-custom">
+        <div class="mt-10 mb-10 md:mt-0">
+          <h2 class="text-center text-lg leading-6 font-medium text-gray-900">
+            Mes valeurs
+          </h2>
+        </div>
+
+        <div class="flex mb-10">
+          <div class="flex-shrink-0">
+            <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+              <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
+              </svg>
             </div>
-          </li>
-          <li class="mt-10 md:mt-0">
-            <div class="flex">
-              <div class="flex-shrink-0">
-                <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                  <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-                  </svg>
-                </div>
-              </div>
-              <div class="ml-4">
-                <h5 class="text-lg leading-6 font-medium text-gray-900">
-                  Mobile notifications
-                </h5>
-                <p class="mt-2 text-base leading-6 text-gray-500">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
-                </p>
-              </div>
+          </div>
+          <div class="ml-4">
+            <h5 class="text-lg leading-6 font-medium text-gray-900">
+              Transfers are instant
+            </h5>
+            <p class="mt-2 text-base leading-6 text-gray-500">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+            </p>
+          </div>
+        </div>
+
+        <div class="flex">
+          <div class="flex-shrink-0">
+            <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+              <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+              </svg>
             </div>
-          </li>
-        </ul>
+          </div>
+          <div class="ml-4">
+            <h5 class="text-lg leading-6 font-medium text-gray-900">
+              Mobile notifications
+            </h5>
+            <p class="mt-2 text-base leading-6 text-gray-500">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style>
+@media screen and (min-width: 768px) {
+  .border-presentation-custom {
+    border-left: dashed 2px #1a202c;
+  }
+}
+</style>
