aaab9bafe83073cb0ac844e2f66e9382c915e8fc
thibaut dumontet <dumontet.thibaut@gmail.com>
2020-05-23 16:31:37 +0200

    Add transitions to homenavbar

--- components/HomeNavbar.vue
+++ components/HomeNavbar.vue
@@ -44,52 +44,54 @@
       From: "opacity-100 scale-100"
       To: "opacity-0 scale-95"
     -->
-    <div v-click-outside="closeMobileNavbar" class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden" :class="{ hidden: !mobileNavOpen }">
-      <div class="rounded-lg shadow-lg">
-        <div class="rounded-lg shadow-xs bg-gray-200">
-          <div class="pt-5 pb-6 px-5 space-y-8">
-            <div class="flex items-center justify-between">
-              <img class="w-24" src="/img/logos/pampam-logo.svg" alt="Logo" />
-              <div class="-mr-2">
-                <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-300 focus:outline-none focus:text-gray-300 transition duration-150 ease-in-out" @click="mobileNavOpen = !mobileNavOpen">
-                  <svg class="h-8 w-8" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                  </svg>
-                </button>
+    <transition name="fade" enter-active-class="duration-200 ease-out" enter-class="opacity-0 scale-95" enter-to-class="opacity-100 scale-100" leave-active-class="duration-100 ease-in" leave-class="opacity-100 scale-100" leave-to-class="opacity-0 scale-95">
+      <div v-show="mobileNavOpen" v-click-outside="closeMobileNavbar" class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+        <div class="rounded-lg shadow-lg">
+          <div class="rounded-lg shadow-xs bg-gray-200">
+            <div class="pt-5 pb-6 px-5 space-y-8">
+              <div class="flex items-center justify-between">
+                <img class="w-24" src="/img/logos/pampam-logo.svg" alt="Logo" />
+                <div class="-mr-2">
+                  <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-300 focus:outline-none focus:text-gray-300 transition duration-150 ease-in-out" @click="mobileNavOpen = !mobileNavOpen">
+                    <svg class="h-8 w-8" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </button>
+                </div>
               </div>
-            </div>
 
-            <nav class="grid row-gap-8">
-              <a href="#" class="-m-3 p-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
-                <div class="text-base leading-6 font-medium text-yellow-600">
-                  Who is Pam Pam?
-                </div>
-              </a>
-              <a href="#" class="-m-3 p-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
-                <div class="text-base leading-6 font-medium text-yellow-600">
-                  La Boutique
-                </div>
-              </a>
-              <a href="#" class="-m-3 p-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
-                <div class="text-base leading-6 font-medium text-yellow-600">
-                  L'Atelier
-                </div>
-              </a>
-              <a href="#" class="-m-3 p-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
-                <div class="text-base leading-6 font-medium text-yellow-600">
-                  Côté Pro
-                </div>
-              </a>
-              <a href="#" class="-m-3 p-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
-                <div class="text-base leading-6 font-medium text-yellow-600">
-                  Journal
-                </div>
-              </a>
-            </nav>
+              <nav class="grid row-gap-8">
+                <a href="#" class="-m-3 p-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
+                  <div class="text-base leading-6 font-medium text-yellow-600">
+                    Who is Pam Pam?
+                  </div>
+                </a>
+                <a href="#" class="-m-3 p-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
+                  <div class="text-base leading-6 font-medium text-yellow-600">
+                    La Boutique
+                  </div>
+                </a>
+                <a href="#" class="-m-3 p-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
+                  <div class="text-base leading-6 font-medium text-yellow-600">
+                    L'Atelier
+                  </div>
+                </a>
+                <a href="#" class="-m-3 p-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
+                  <div class="text-base leading-6 font-medium text-yellow-600">
+                    Côté Pro
+                  </div>
+                </a>
+                <a href="#" class="-m-3 p-3 rounded-md hover:bg-gray-50 transition ease-in-out duration-150">
+                  <div class="text-base leading-6 font-medium text-yellow-600">
+                    Journal
+                  </div>
+                </a>
+              </nav>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </transition>
   </section>
 </template>
 
