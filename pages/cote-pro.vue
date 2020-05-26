679b883e8b065d9df4176b70d6e5ac4417258876
thibaut dumontet <dumontet.thibaut@gmail.com>
2020-05-26 21:50:51 +0200

    create cote-pro page with first dummy grid implementation

---
+++ pages/cote-pro.vue
@@ -0,0 +1,14 @@
+<template>
+  <cote-pro-section-one />
+</template>
+
+<script>
+import CoteProSectionOne from '~/components/CoteProSectionOne.vue'
+export default {
+  components: {
+    CoteProSectionOne
+  }
+}
+</script>
+
+<style></style>
