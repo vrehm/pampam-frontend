f22dc91ced18e16718d5be81a14e78f6b5bafee1
thibaut dumontet <dumontet.thibaut@gmail.com>
2020-05-27 18:20:19 +0200

    Add decoration section to cote pro page

--- components/CoteProSectionOne.vue
+++ components/CoteProSectionOne.vue
@@ -24,14 +24,6 @@
           </div>
 
           <div class="mt-10 -mx-4 relative lg:mt-0 lg:col-start-1">
-            <svg class="absolute left-1/2 transform -translate-x-1/2 translate-y-16 lg:hidden" width="784" height="404" fill="none" viewBox="0 0 784 404">
-              <defs>
-                <pattern id="e80155a9-dfde-425a-b5ea-1f6fadd20131" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-                  <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-                </pattern>
-              </defs>
-              <rect width="784" height="404" fill="url(#e80155a9-dfde-425a-b5ea-1f6fadd20131)" />
-            </svg>
             <img class="relative mx-auto" width="490" src="/img/cote-pro/dessin-pro.jpg" alt="dessin de l'activité pro de PamPam" />
           </div>
         </div>
@@ -51,14 +43,6 @@
           </div>
 
           <div class="mt-10 -mx-4 relative lg:mt-0 lg:col-start-1">
-            <svg class="absolute left-1/2 transform -translate-x-1/2 translate-y-16 lg:hidden" width="784" height="404" fill="none" viewBox="0 0 784 404">
-              <defs>
-                <pattern id="e80155a9-dfde-425a-b5ea-1f6fadd20131" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-                  <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-                </pattern>
-              </defs>
-              <rect width="784" height="404" fill="url(#e80155a9-dfde-425a-b5ea-1f6fadd20131)" />
-            </svg>
             <img class="relative mx-auto rounded-sm" width="490" src="/img/cote-pro/photo-location.jpg" alt="Photo du service pro de location de l'atelier Pampam" />
           </div>
         </div>
@@ -66,80 +50,20 @@
 
       <!-- left version -->
 
-      <div class="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
-        <div class="relative">
-          <h4 class="text-2xl leading-8 font-extrabold text-gray-900 tracking-tight sm:text-3xl sm:leading-9">
-            Transfer funds world-wide
-          </h4>
-          <p class="mt-3 text-lg leading-7 text-gray-500">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur minima sequi recusandae, porro maiores officia assumenda aliquam laborum ab aliquid veritatis impedit odit adipisci optio iste blanditiis facere. Totam, velit.
-          </p>
-
-          <ul class="mt-10">
-            <li>
-              <div class="flex">
-                <div class="flex-shrink-0">
-                  <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
-                    </svg>
-                  </div>
-                </div>
-                <div class="ml-4">
-                  <h5 class="text-lg leading-6 font-medium text-gray-900">Competitive exchange rates</h5>
-                  <p class="mt-2 text-base leading-6 text-gray-500">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
-                  </p>
-                </div>
-              </div>
-            </li>
-            <li class="mt-10">
-              <div class="flex">
-                <div class="flex-shrink-0">
-                  <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
-                    </svg>
-                  </div>
-                </div>
-                <div class="ml-4">
-                  <h5 class="text-lg leading-6 font-medium text-gray-900">No hidden fees</h5>
-                  <p class="mt-2 text-base leading-6 text-gray-500">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
-                  </p>
-                </div>
-              </div>
-            </li>
-            <li class="mt-10">
-              <div class="flex">
-                <div class="flex-shrink-0">
-                  <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
-                    </svg>
-                  </div>
-                </div>
-                <div class="ml-4">
-                  <h5 class="text-lg leading-6 font-medium text-gray-900">Transfers are instant</h5>
-                  <p class="mt-2 text-base leading-6 text-gray-500">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
-                  </p>
-                </div>
-              </div>
-            </li>
-          </ul>
-        </div>
-
-        <div class="mt-10 -mx-4 relative lg:mt-0">
-          <svg class="absolute left-1/2 transform -translate-x-1/2 translate-y-16 lg:hidden" width="784" height="404" fill="none" viewBox="0 0 784 404">
-            <defs>
-              <pattern id="ca9667ae-9f92-4be7-abcb-9e3d727f2941" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
-                <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-              </pattern>
-            </defs>
-            <rect width="784" height="404" fill="url(#ca9667ae-9f92-4be7-abcb-9e3d727f2941)" />
-          </svg>
-          <img class="relative mx-auto" width="490" src="/img/features/feature-example-1.png" alt="" />
+      <div class="relative mt-12 sm:mt-16 lg:mt-24">
+        <div class="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
+          <div class="mt-10 -mx-4 relative lg:mt-0 lg:col-start-2">
+            <img class="relative mx-auto rounded-sm" width="490" src="/img/cote-pro/photo-decoration.jpg" alt="Photo du service pro de décoration de l'atelier Pampam" />
+          </div>
+          <div class="lg:col-start-1">
+            <h4 class="text-2xl leading-8 font-extrabold text-gray-900 tracking-tight sm:text-3xl sm:leading-9">
+              Vous avec besoin d’être épaulé dans la création d’une ambiance unique et sur mesure pour votre projet?
+            </h4>
+            <p class="mt-4 max-w-3xl text-xl leading-7 text-gray-500">-Appelez moi, et discutons en ensemble de vive voix! -RDV de 2h à 3h sur les lieux: Analyse de vos besoins, conseil et évaluation du projet, premières idées, prise de mesures et photo. -Restitution d’une proposition d’aménagement: un devis d’ameublement/décoration complet + dessin de principe</p>
+            <p class="mt-4 max-w-3xl text-xl leading-7 text-gray-700">
+              Tarif: 380€.
+            </p>
+          </div>
         </div>
       </div>
     </div>
