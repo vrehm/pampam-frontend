e0f1ff918b191d23e04a495c19f5721507cc573c
thibaut dumontet <dumontet.thibaut@gmail.com>
2020-05-29 11:40:24 +0200

    Fix navbar links & add animatecss library

--- components/CoteProEspaceSection.vue
+++ components/CoteProEspaceSection.vue
@@ -3,16 +3,7 @@
     <div class="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-screen-xl">
       <div class="relative mt-12 sm:mt-16 lg:mt-24">
         <div class="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
-          <!-- <div class="lg:col-start-2">
-            <h4 class="text-2xl leading-8 font-extrabold text-gray-900 tracking-tight sm:text-3xl sm:leading-9">
-              Vous recherchez un bel espace atypique pour vos shootings photo?
-            </h4>
-            <p class="mt-4 max-w-3xl text-xl leading-7 text-gray-500">Je loue mon showroom de 60m2, style atelier/brocante, très lumineux, 5m de hauteur sous plafond, avec mobilier à disposition pour mise en scène sur demande et inclus.<br /></p>
-            <p class="mt-4 max-w-3xl text-xl leading-7 text-gray-700">
-              Tarifs: 50€ HT de l’heure - 150€ HT la demi-journée (4h) - 250€ HT la journée (8h)
-            </p>
-          </div> -->
-          <div class="lg:col-start-2">
+          <div class="col-start-1 lg:col-start-2">
             <div class="bg-white px-6 py-8 lg:flex-shrink-1 lg:p-12">
               <h3 class="text-2xl leading-8 font-extrabold text-gray-900 sm:text-3xl sm:leading-9">
                 Vous recherchez un bel espace atypique pour vos shootings photo?
@@ -115,7 +106,7 @@
             </div>
           </div>
 
-          <div class="mt-10 -mx-4 relative lg:mt-0 lg:col-start-1">
+          <div class="mt-10 -mx-4 relative lg:mt-0 col-start-2 lg:col-start-1">
             <img class="relative mx-auto lg:rounded-sm" width="490" src="/img/cote-pro/photo-stylisme.jpg" alt="Photo du service pro de location de l'atelier Pampam" />
             <div class="absolute flex inset-0 items-center justify-center flex-col">
               <svg width="61" height="58" viewBox="0 0 61 58" fill="none" xmlns="http://www.w3.org/2000/svg">
