4cd45566315cfac89b8702063d689da6beedb0f6
vrehm <vincentrehm@yahoo.fr>
2020-05-31 13:42:46 +0200

    fix deprecated scrollBehavior

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,33 +12,34 @@ export default {
      ** Router configuration
      */
     router: {
-        middleware: ['password-protect'],
-        scrollBehavior: async(to, from, savedPosition) => {
-            if (savedPosition) {
-                return savedPosition
-            }
+        middleware: ['password-protect']
 
-            const findEl = (hash, x) => {
-                return document.querySelector(hash) ||
-                    new Promise((resolve, reject) => {
-                        if (x > 50) {
-                            return resolve()
-                        }
-                        setTimeout(() => { resolve(findEl(hash, ++x || 1)) }, 100)
-                    })
-            }
+        // scrollBehavior: async(to, from, savedPosition) => {
+        //     if (savedPosition) {
+        //         return savedPosition
+        //     }
 
-            if (to.hash) {
-                const el = await findEl(to.hash)
-                if ('scrollBehavior' in document.documentElement.style) {
-                    return window.scrollTo({ top: el.offsetTop, behavior: 'smooth' })
-                } else {
-                    return window.scrollTo(0, el.offsetTop)
-                }
-            }
+        //     const findEl = (hash, x) => {
+        //         return document.querySelector(hash) ||
+        //             new Promise((resolve, reject) => {
+        //                 if (x > 50) {
+        //                     return resolve()
+        //                 }
+        //                 setTimeout(() => { resolve(findEl(hash, ++x || 1)) }, 100)
+        //             })
+        //     }
 
-            return { x: 0, y: 0 }
-        }
+        //     if (to.hash) {
+        //         const el = await findEl(to.hash)
+        //         if ('scrollBehavior' in document.documentElement.style) {
+        //             return window.scrollTo({ top: el.offsetTop, behavior: 'smooth' })
+        //         } else {
+        //             return window.scrollTo(0, el.offsetTop)
+        //         }
+        //     }
+
+        //     return { x: 0, y: 0 }
+        // }
     },
     server: {
         host: process.env.MODE === 'production' ? '0.0.0.0' : 'localhost', // default: localhost
