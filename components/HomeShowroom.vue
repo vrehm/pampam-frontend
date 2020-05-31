54e4bfbc465445b88a339644bc97b29854c74d9e
kyldex <raphael.giagnorio@gmail.com>
2020-05-31 22:34:06 +0200

    commit home changes before pulling optimizing-depencies

---
+++ components/HomeShowroom.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="pt-6 pb-12 sm:pb-8 md:pt-12 md:pb-20 bg-white">
+    <div class="max-w-screen-xl mt-10 mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex flex-col mt-14 md:flex-row md:justify-center md:mt-16">
+        <!-- Left block -->
+        <div class="flex flex-col mt-12 md:mt-0 w-full md:w-6/12 md:pr-8">
+          <div class="mx-auto w-11/12 lg:w-8/12">
+            <div class="flex justify-between">
+              <div class="text-xl italic leading-8 tracking-tight text-gray-200 md:text-2xl md:leading-9">
+                illustration
+              </div>
+              <div class="text-xl italic leading-8 tracking-tight text-gray-200 md:text-2xl md:leading-9">
+                illustration
+              </div>
+            </div>
+          </div>
+
+          <div class="mx-auto w-11/12 lg:w-8/12">
+            <img class="w-full rounded-md lazyload" data-src="~/assets/img/home/textile1-700x733.jpg" alt="portrait" />
+          </div>
+        </div>
+
+        <!-- Right block -->
+        <div class="w-full md:w-6/12 md:pl-8">
+          <div class="w-11/12 mx-auto sm:w-full">
+            <h2 class="text-center text-2xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-3xl sm:leading-9">
+              Le Showroom
+            </h2>
+          </div>
+
+          <div class="mt-10 w-11/12 mx-auto sm:w-full">
+            <p class="mt-2 text-base text-justify leading-6 text-gray-900">
+              Mon premier amour ! Ayant débuté comme tapissière, je suis passionnée par cette matière, au travers du toucher, des couleurs, des motifs, j’adore l’incorporer un peu partout et imaginer de nouveaux prototypes d’objets en utilisant notamment des linges et étoffes anciennes.
+            </p>
+            <p class="mt-2 text-base text-justify leading-6 text-gray-900">
+              Coussins, poufs, suspensions végétales ou encore toiles murales. Je travaille en séries limitées ou pièces uniques, retrouvez mes créations assignées de la mention “Made by Atelier Pam Pam” sur ma boutique Instagram et à l’atelier !
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
