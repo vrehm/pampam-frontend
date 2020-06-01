c3e015472ab43d45a6f70c634af0d608f3cdc8ba
kyldex <raphael.giagnorio@gmail.com>
2020-06-02 00:50:17 +0200

    home showroom v1 integration

--- components/HomeShowroom.vue
+++ components/HomeShowroom.vue
@@ -1,10 +1,14 @@
 <template>
   <section class="pt-6 pb-12 sm:pb-8 md:pt-12 md:pb-20 bg-white">
-    <div class="max-w-screen-xl mt-10 mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="flex flex-col mt-14 md:flex-row md:justify-center md:mt-16">
+    <div class="max-w-screen-lg mt-10 mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex flex-col md:flex-row md:justify-center mt-0 md:mt-16">
         <!-- Left block -->
-        <div class="flex flex-col mt-12 md:mt-0 w-full md:w-6/12 md:pr-8">
-          <div class="mx-auto w-11/12 lg:w-8/12">
+        <div class="flex flex-col mt-0 ml-0 sm:mt-10 md:mt-0 xl:mr-4 w-full md:w-6/12">
+          <h2 class="text-center text-2xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-3xl sm:leading-9 md:hidden">
+            Le showroom
+          </h2>
+
+          <div class="mt-12 mx-auto md:mt-0 w-11/12 sm:w-9/12 md:w-11/12">
             <div class="flex justify-between">
               <div class="text-xl italic leading-8 tracking-tight text-gray-200 md:text-2xl md:leading-9">
                 illustration
@@ -15,23 +19,38 @@
             </div>
           </div>
 
-          <div class="mx-auto w-11/12 lg:w-8/12">
-            <img class="w-full rounded-md lazyload" data-src="~/assets/img/home/textile1-700x733.jpg" alt="portrait" />
+          <div class="mx-auto w-11/12 sm:w-9/12 md:w-11/12">
+            <img class="w-full max-h-custom-showroom object-cover rounded-md lazyload" data-src="~/assets/img/home/showroom-1-700x933.jpg" alt="Photo showroom" />
           </div>
         </div>
 
         <!-- Right block -->
-        <div class="w-full md:w-6/12 md:pl-8">
-          <div class="w-11/12 mx-auto sm:w-full">
-            <h2 class="text-center text-2xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-3xl sm:leading-9">
-              Le Showroom
+        <div class="ml-0 xl:ml-4 w-full md:w-6/12">
+          <div class="mx-auto pt-0 md:pt-16 lg:pt-16 xl:pt-20">
+            <h2 class="hidden md:block text-center text-2xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-3xl sm:leading-9">
+              Le showroom
             </h2>
-          </div>
 
-          <div class="mt-10 w-11/12 mx-auto sm:w-full">
-            <p class="mt-2 text-base text-justify leading-6 text-gray-900">
-              Mon premier amour ! Ayant débuté comme tapissière, je suis passionnée par cette matière, au travers du toucher, des couleurs, des motifs, j’adore l’incorporer un peu partout et imaginer de nouveaux prototypes d’objets en utilisant notamment des linges et étoffes anciennes.
-            </p>
+            <div class="flex flex-col mt-8 md:mt-12">
+              <div>
+                <h3 class="text-center text-xl sm:text-2xl font-extrabold leading-8 sm:leading-9 tracking-tight text-gray-900">
+                  Adresse
+                </h3>
+                <p class="mt-2 text-center text-base leading-6 text-gray-900">
+                  18 bis Avenue Henri Barbusse<br />
+                  69250 Albigny-sur -Saône
+                </p>
+              </div>
+              <div class="mt-6">
+                <h3 class="text-center text-xl sm:text-2xl font-extrabold leading-8 sm:leading-9 tracking-tight text-gray-900">
+                  Horaires
+                </h3>
+                <p class="mt-2 text-center text-base leading-6 text-gray-900">
+                  Ouvert du Mardi au Samedi<br class="sm:hidden" />
+                  sur rendez-vous !
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -43,4 +62,32 @@
 export default {}
 </script>
 
-<style></style>
+<style>
+.max-h-custom-showroom {
+  max-height: 350px;
+}
+
+@media (min-width: 640px) {
+  .max-h-custom-showroom {
+    max-height: 400px;
+  }
+}
+
+@media (min-width: 768px) {
+  .max-h-custom-showroom {
+    max-height: 350px;
+  }
+}
+
+@media (min-width: 1024px) {
+  .max-h-custom-showroom {
+    max-height: 450px;
+  }
+}
+
+@media (min-width: 1280px) {
+  .max-h-custom-showroom {
+    max-height: 500px;
+  }
+}
+</style>
