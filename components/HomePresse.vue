375aadf676970681299a42b31c2c5336bff94493
vrehm <vincentrehm@yahoo.fr>
2020-06-05 15:12:25 +0200

    fix _blanck typo to _blank

--- components/HomePresse.vue
+++ components/HomePresse.vue
@@ -11,17 +11,17 @@
       </div>
       <div class="flex flex-col items-center md:flex-row md:justify-around pt-14">
         <div class="flex justify-center w-3/5 md:w-1/4 lg:w-1/5">
-          <a href="https://www.tiffanyoliverphotographies.com/blog-photographe-mariage-lyon/mes-brocs-ont-du-talent-photographe-puces-du-canal-a-lyon-atelier-pampam" target="_blanck">
+          <a href="https://www.tiffanyoliverphotographies.com/blog-photographe-mariage-lyon/mes-brocs-ont-du-talent-photographe-puces-du-canal-a-lyon-atelier-pampam" target="_blank">
             <img class="lazyload" data-src="~/assets/img/logos/logo-tiffany-photographies.jpg" alt="Tiffany Photographies" />
           </a>
         </div>
         <div class="flex justify-center mt-14 md:mt-0 w-3/5 md:w-1/4 lg:w-1/5">
-          <a href="https://www.mylittlelyon.com/deco/la-brocante-qui-joue-a-cache-cache" target="_blanck">
+          <a href="https://www.mylittlelyon.com/deco/la-brocante-qui-joue-a-cache-cache" target="_blank">
             <img class="lazyload" data-src="~/assets/img/logos/logo-my-little-lyon.png" alt="My Little Lyon" />
           </a>
         </div>
         <div class="flex justify-center p-2 mt-14 md:mt-0 w-3/5 md:w-1/4 lg:w-1/5">
-          <a href="https://nouvellesrepliques.wordpress.com/2017/02/28/loch-nessbroctheatre-au-theatre-des-voraces-lyon/" target="_blanck">
+          <a href="https://nouvellesrepliques.wordpress.com/2017/02/28/loch-nessbroctheatre-au-theatre-des-voraces-lyon/" target="_blank">
             <img class="lazyload" data-src="~/assets/img/logos/logo-nouvelles-repliques.jpg" alt="Nouvelles Répliques" />
           </a>
         </div>
