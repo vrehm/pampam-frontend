94161d14503f21ee14eb067e6ea8274f56dab121
thibaut dumontet <dumontet.thibaut@gmail.com>
2020-06-07 20:22:44 +0200

    Add content place holder to show & replace slug by id

--- components/ArticleShow.vue
+++ components/ArticleShow.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="my-4">
-    <template v-if="$fetchState.pending">
-      <content-placeholders rounded>
-        <content-placeholders-heading />
-        <content-placeholders-img />
-        <content-placeholders-text :lines="50" />
-      </content-placeholders>
-    </template>
-    <template v-else-if="$fetchState.error">
-      <p>{{ $fetchState.error }}</p>
-    </template>
-    <template v-else>
-      <img :src="assetsBaseUrl + article.image.url" :alt="article.title" />
-      <div class="text-gray-700 text-base break-normal my-4 leading-loose mx-auto max-w-4xl">
-        {{ article.content }}
-      </div>
-    </template>
+  <div class="relative pt-10 pb-12 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32 w-full">
+    <div class="mt-10 mx-auto max-w-screen-xl lg:px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
+      <template v-if="$fetchState.pending">
+        <content-placeholders rounded class="pb-8 px-6 md:px-24 md:max-w-5xl  md:px-24 mx-auto w-4/5">
+          <content-placeholders-heading :img="true" />
+          <content-placeholders-img />
+          <content-placeholders-text :lines="20" />
+        </content-placeholders>
+      </template>
+      <template v-else-if="$fetchState.error">
+        <p>{{ $fetchState.error }}</p>
+      </template>
+      <template v-else>
+        <img :src="assetsBaseUrl + article.image.url" :alt="article.title" />
+        <div class="text-gray-700 text-base break-normal my-4 leading-loose mx-auto max-w-4xl">
+          {{ article.content }}
+        </div>
+      </template>
+    </div>
   </div>
 </template>
 
