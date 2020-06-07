edb0ef01a8aba5921722cf9b63fb507ce4c4c45c
thibaut dumontet <dumontet.thibaut@gmail.com>
2020-06-07 12:51:19 +0200

    Add logic to observer to call a method

--- pages/journal.vue
+++ pages/journal.vue
@@ -4,7 +4,7 @@
       <journal-hero-section />
       <div class="relative pt-10 pb-12 sm:pb-16 md:pb-20 lg:pb-28 xl:pb-32">
         <div class="mt-10 mx-auto max-w-screen-xl lg:px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
-          <article-card v-for="article in articles" :key="article.id" v-observe-visibility="i === articles.length - 1 ? lazyLoadArticles : false" :article="article" class="flex" />
+          <article-card v-for="(article, i) in articles" :key="article.id" v-observe-visibility="i === articles.length - 1 ? lazyLoadArticles : false" :article="article" class="flex" />
         </div>
         <!-- <the-more-article-button v-if="articlesCount > 5" @load-articles="fiveMoreArticles" /> -->
       </div>
@@ -80,6 +80,15 @@ export default {
     //     const newArticles = await this.$axios.$get(this.baseURL + '/articles?_start=10&_limit=5')
     //     this.articles.push(...newArticles)
     //   }
+    lazyLoadArticles(isVisible) {
+      if (isVisible) {
+        if (this.currentPage < 5) {
+          this.currentPage++
+          console.log('+1')
+          // this.$fetch()
+        }
+      }
+    }
   }
 }
 </script>
