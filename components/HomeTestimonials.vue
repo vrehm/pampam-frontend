7f8fe2cb394bdab3a3252f1538a2277681342fc9
kyldex <raphael.giagnorio@gmail.com>
2020-06-23 20:49:51 +0200

    setup responsive carousel for home testimonials

--- components/HomeTestimonials.vue
+++ components/HomeTestimonials.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="bg-gray-100">
-    <div class="max-w-lg md:max-w-screen-xl mx-auto sm:pb-6 px-6 lg:px-8">
+    <div class="max-w-lg md:max-w-screen-xl mx-auto sm:pb-6 px-8 sm:px-6 md:px-12 xl:px-8">
       <VueSlickCarousel v-bind="settings">
-        <testimonial-card v-for="customer in customers" :key="customer.id" class="py-20" :arrows="true" :name="customer.name" :testimony="customer.testimony" :testimony2="customer.testimony2" :customer-img="customer.customerImg" />
+        <testimonial-card v-for="customer in customers" :key="customer.id" class="focus:outline-none py-20" :name="customer.name" :testimony="customer.testimony" :testimony2="customer.testimony2" :customer-img="customer.customerImg" />
       </VueSlickCarousel>
     </div>
   </section>
@@ -12,7 +12,7 @@
 import VueSlickCarousel from 'vue-slick-carousel'
 import 'vue-slick-carousel/dist/vue-slick-carousel.css'
 // optional style for arrows & dots
-// import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 import TestimonialCard from '~/components/TestimonialCard.vue'
 
 export default {
@@ -26,12 +26,22 @@ export default {
       arrows: true,
       dots: false,
       infinite: true,
-      initialSlide: 2,
-      speed: 2000,
-      slidesToShow: 3,
-      slidesToScroll: 1,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      initialSlide: 0,
+      speed: 1500,
       swipeToSlide: true,
-      autoplay: true
+      autoplay: true,
+      focusOnSelect: true,
+      responsive: [
+        {
+          breakpoint: 740,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
     },
     customers: [
       {
