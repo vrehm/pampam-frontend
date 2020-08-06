65ed1731747bba606795493c354d5ee73368ba9a
thibaut dumontet <dumontet.thibaut@gmail.com>
2020-08-06 19:13:28 +0200

    style - journal navbar

--- components/JournalNavBar.vue
+++ components/JournalNavBar.vue
@@ -13,10 +13,10 @@
     </div>
     <div class="hidden sm:block">
       <nav class="flex justify-center">
-        <nuxt-link no-prefetch class="px-3 py-2 font-light font-bold text-sm leading-5 rounded-md text-gray-700 hover:text-gray-900 focus:outline-none" :to="{ name: 'journal' }">
+        <nuxt-link no-prefetch class="px-3 py-2 font-light font-bold text-sm leading-5 rounded-sm text-gray-700 hover:text-gray-900 focus:outline-none" :to="{ name: 'journal' }">
           Tous
         </nuxt-link>
-        <nuxt-link v-for="category in categories" :key="category.id" no-prefetch class="ml-4 px-3 py-2 font-light font-bold text-sm leading-5 rounded-md text-gray-700 hover:text-gray-900 focus:outline-none" :to="{ name: 'categories-category', params: { category: category.slug } }">
+        <nuxt-link v-for="category in categories" :key="category.id" no-prefetch class="ml-4 px-3 py-2 font-light font-bold text-sm leading-5 rounded-sm text-gray-700 hover:text-gray-900 focus:outline-none" :to="{ name: 'categories-category', params: { category: category.slug } }">
           {{ category.name }}
         </nuxt-link>
       </nav>
