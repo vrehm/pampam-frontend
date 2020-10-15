cd6d80798ca0ba61dde3240503cf29e1284056c3
vrehm <vincentrehm@yahoo.fr>
2020-10-15 17:17:35 +0200

    add mobile version for fixed hamburger menu

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -7,11 +7,11 @@
       <div class="max-w-7xl mx-auto px-4 sm:px-6">
         <div class="flex justify-between items-center md:items-start py-6 md:space-x-10">
           <nuxt-link to="/">
-            <img :class="{ 'w-24 md:w-32 lg:w-48': heroVisibility, 'md:fixed md:w-20': !heroVisibility }" src="~/assets/img/logos/pampam-logo.svg" alt="Logo" />
+            <img :class="{ 'w-24 md:w-32 lg:w-48': heroVisibility, 'md:fixed w-24 md:w-20': !heroVisibility }" src="~/assets/img/logos/pampam-logo.svg" alt="Logo" />
           </nuxt-link>
-          <div class="-mr-2 -my-2 md:hidden">
+          <div class="-mr-2 -my-2 md:hidden fixed right-0 top-0 pt-12 pr-6">
             <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-200 hover:text-gray-400 focus:outline-none focus:text-gray-400 transition duration-150 ease-in-out" @click.prevent="mobileNavOpen = !mobileNavOpen">
-              <svg class="h-8 w-8" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+              <svg class="h-8 w-8 bg-gray-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
               </svg>
             </button>
@@ -31,9 +31,9 @@
         <nuxt-link to="/">
           <img :class="{ 'w-24 md:w-32 lg:w-48': nonHomeHeroVisibility, 'md:fixed md:w-20': !nonHomeHeroVisibility }" src="~/assets/img/logos/pampam-logo.svg" alt="Logo" />
         </nuxt-link>
-        <div class="-mr-2 -my-2 md:hidden">
+        <div class="-mr-2 -my-2 md:hidden fixed right-0 top-0 pt-12 pr-6">
           <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-200 hover:text-gray-400 focus:outline-none focus:text-gray-400 transition duration-150 ease-in-out" @click.prevent="mobileNavOpen = !mobileNavOpen">
-            <svg class="h-8 w-8" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+            <svg class="h-8 w-8 bg-gray-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
           </button>
@@ -48,7 +48,7 @@
 
     <!-- Mobile version -->
     <transition name="fade" enter-active-class="duration-200 ease-out" enter-class="opacity-0 scale-95" enter-to-class="opacity-100 scale-100" leave-active-class="duration-100 ease-in" leave-class="opacity-100 scale-100" leave-to-class="opacity-0 scale-95">
-      <div v-show="mobileNavOpen" v-click-outside="closeMobileNavbar" class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden z-50">
+      <div v-show="mobileNavOpen" v-click-outside="closeMobileNavbar" class="fixed top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden z-50">
         <div class="rounded-lg shadow-lg">
           <div class="rounded-lg shadow-xs bg-white">
             <div class="pt-5 pb-6 px-5 space-y-8">
